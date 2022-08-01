5fc18a25c6bd0b6b5225fb0e079f771b7ee8b766
qbentil <bentilshadrack72@gmail.com>
2022-08-01 18:27:45 +0000

    Init body

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/navbar";
+import Home from "./components/home";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ src/components/home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="w-screen h-[90vh] bg-black">
+        
+    </div>
+  )
+}
+
+export default Home
