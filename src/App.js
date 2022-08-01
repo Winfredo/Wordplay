f1eeaf0d4b0c1bfe00fb9ef985a5cfb45ac52ad9
Winfred Nukpezah <winfrednukpe2002@gmail.com>
2022-08-01 17:34:41 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <div>
-      <h2 className='text-red-500 text-3xl underline font-bold'>Winferedooooo</h2>
+      <Navbar />
     </div>
   );
 }
