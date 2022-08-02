e5f3d8e32066cb8f91fa6224cd379a22c1d60ded
Winfred Nukpezah <winfrednukpe2002@gmail.com>
2022-08-02 00:22:37 +0000

    creating a login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
+
 function LoginPage() {
   return (
-    <div>LoginPage</div>
+    <div className='w-screen h-screen bg-[#1C1C1C] flex flex-col justify-center items-center '  >
+      <div className='text-[#FFBE32] text-[9rem]'>
+        <h2 className='font-text title'>WeWrite</h2>
+      </div>
+      <div className='text-[#FFBE32] text-6xl font-bold pb-10'>
+        <p> You are logged in</p>
+      </div>
+      <div className='text-[#FFBE32] w-fit  cursor-pointer rounded-full px-5 py-1 text-3xl font-bold bg-[#1C1C0B ] shadow-lg shadow-[#1C1C0B]-500/10' >
+      <button> Log In</button>
+      </div>
+    </div>
   )
 }
 
