51b1b24a2b774e9f3843c8b49c17b22243604d8f
Winfred Nukpezah <winfrednukpe2002@gmail.com>
2022-08-05 03:10:58 +0000

    commit

--- src/components/write/index.js
+++ src/components/write/index.js
@@ -3,11 +3,22 @@ import React from 'react'
 
 function Write() {
   return (
-    <div>
-      <p>Winfred</p>
-    </div>
+          <div className='w-screen h-[90vh] text-primary bg-dark-bg flex flex-col justify-center items-center'>
+            <div className='title font-semibold text-[5rem]'>
+              <h2>Write</h2>
+               </div>
+               <div className=''></div>
+               <div>
+               <button className='font-semibold text-4xl shadow-lg'>New Sentence</button>
+
+               </div>
+          </div>
+      
+    
     
   )
+
+  
 }
 
 export default Write
